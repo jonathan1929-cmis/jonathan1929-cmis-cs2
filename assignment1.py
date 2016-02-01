0b0d75c4d0335bfabbd256e05e13630f412e78e8
cmis <cmis@cmis-hs23.(none)>
2016-02-01 13:36:17 +0700

    Finished assignment

---
+++ assignment1.py
@@ -0,0 +1,20 @@
+MyName = "Jonathan"
+MyAge = 16.91
+MyHeight = 1.75
+SideSquare = 2
+RectangleL = 5
+RectangleH = 3
+AgeMonths = MyAge*12
+AgeLeft = 71.5-MyAge
+MyHeightFeet = MyHeight*3.28
+DifferenceAvHeightMeters = 1.675-MyHeight
+SquareArea = SideSquare**2
+HalfVolCube = SquareArea*SideSquare
+NinthAreaRect = (RectangleL*RectangleH)/9.0
+print "The Side of the square is " + str(SideSquare) + ". Therefore, the Area of the Square is " + str(SquareArea) +". This would then mean half of the volume would be " + str(HalfVolCube) +".  A rectangle is different, their dimensions are " + str(RectangleL) +"x"+ str(RectangleH) + " and 1/9 of the volume is " + str(NinthAreaRect) +"."
+print "Hello. My name is " , MyName , "."
+print "I am " , MyHeight , " meters tall."
+print "I am " , MyAge , " years old so I have approximately " , AgeLeft , " years remaining in my life."
+print "I am " , MyHeightFeet , " tall in feet."
+
+
