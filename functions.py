d59b45eae26d12dcd9833e7803829e90a15cdeda
cmis <cmis@cmis-hs23.(none)>
2016-02-05 13:35:25 +0700

    Testing

---
+++ functions.py
@@ -0,0 +1,21 @@
+def advice():
+	print "Sharing is caring!"
+	print "Share all your corrosive urns!"
+	print "It's easy! Just throw it at your companion!"
+	print "He/She will be very thankful!"
+
+advice()
+def favfood(food):
+	print "My favorite food is %s!" % food
+
+favfood("cake")
+
+def enlighten(food):
+	print "The cake is a lie!"
+
+
+
+enlighten("dog")
+
+def console(thing1, option1, option2)
+	print "Just relax! Turn on the %s!" % thing1
