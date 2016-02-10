5f4ea04bc07b0e72b4c6559a76c6eaee306b0761
cmis <cmis@cmis-hs23.(none)>
2016-02-10 10:38:45 +0700

    Added comments and Faces

--- assignment1.py
+++ assignment1.py
@@ -1,20 +1,21 @@
-MyName = "Jonathan"
-MyAge = 16.91
-MyHeight = 1.75
-SideSquare = 2
-RectangleL = 5
-RectangleH = 3
-AgeMonths = MyAge*12
-AgeLeft = 71.5-MyAge
-MyHeightFeet = MyHeight*3.28
-DifferenceAvHeightMeters = 1.675-MyHeight
-SquareArea = SideSquare**2
-HalfVolCube = SquareArea*SideSquare
-NinthAreaRect = (RectangleL*RectangleH)/9.0
-print "The Side of the square is " + str(SideSquare) + ". Therefore, the Area of the Square is " + str(SquareArea) +". This would then mean half of the volume would be " + str(HalfVolCube) +".  A rectangle is different, their dimensions are " + str(RectangleL) +"x"+ str(RectangleH) + " and 1/9 of the volume is " + str(NinthAreaRect) +"."
-print "Hello. My name is " , MyName , "."
-print "I am " , MyHeight , " meters tall."
-print "I am " , MyAge , " years old so I have approximately " , AgeLeft , " years remaining in my life."
-print "I am " , MyHeightFeet , " tall in feet."
+MyName = "Jonathan" #The one and only me
+MyAge = 16.91 #My age in years
+MyHeight = 1.75 #My height in meters
+SideSquare = 2 #Length of 1 side of a square
+RectangleL = 5 #Length of a Rectangle
+RectangleH = 3 #Height of the same Rectangle
+AgeMonths = MyAge*12 #My age in months
+AgeLeft = 71.5-MyAge #How much time I have left to live
+MyHeightFeet = MyHeight*3.28 #My height in feet
+DifferenceAvHeightMeters = 1.675-MyHeight #THe difference of my height from the average height.
+SquareArea = SideSquare**2 #The area of the square from the sides given
+HalfVolCube = SquareArea*SideSquare #Half the volume of the previous cube
+NinthAreaRect = (RectangleL*RectangleH)/9.0 #1/9 the area of the rectangle with the given sides
+Smiley = " :3" #dem smiley faces doe
+print "The Side of the square is " + str(SideSquare) + ". Therefore, the Area of the Square is " + str(SquareArea) +". This would then mean half of the volume would be " + str(HalfVolCube) +".  A rectangle is different, their dimensions are " + str(RectangleL) +"x"+ str(RectangleH) + " and 1/9 of the volume is " + str(NinthAreaRect) +"." #Sentence with 5 variables within it
+print "Hello. My name is " , MyName , "." #Introduction using variables
+print "I am " , MyHeight , " meters tall." #Same method explaining my height
+print "I am " , MyAge , " years old so I have approximately " , AgeLeft , " years remaining in my life." #My age and how much time I have remaining
+print "I am " , MyHeightFeet , " tall in feet." #Explaining my heigh in feet
 
-
+print Smiley*10000 #LEGION OF CAT FACES
