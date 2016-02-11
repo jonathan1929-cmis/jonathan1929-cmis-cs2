9be59de0ea7ed14350aff62b37be65de7d947b07
cmis <cmis@cmis-hs23.(none)>
2016-02-11 13:35:56 +0700

    worked on the functions assignment

--- functions.py
+++ functions.py
@@ -1,21 +1,23 @@
-def advice():
-	print "Sharing is caring!"
-	print "Share all your corrosive urns!"
-	print "It's easy! Just throw it at your companion!"
-	print "He/She will be very thankful!"
+import math
+def add(a, b):
+	return a + b #This functions adds a and b
+def sub(a, b):
+	return a - b #This function subtracts b from a
+def mul(a, b):
+	return a * b #This function multiplies a by b
+def div(a, b):
+	return a / b #This function divide b by a
+def hours_from_seconds(a):
+	return a / 60 #This function takes the seconds and divides it by 60 to give you the number of hours that amount of seconds represent
+def circle_area(r):
+	pi = (math.pi)	
+	return pi * r**2 #This function takes radius of a circle and converts it to the area of a circle.
+def sphere_volume(r):
+	pi = (math.pi)
+	return pi * r**3 * 4/3 #This function takes the radius of a circle and convert it to the volume of that circle.
 
-advice()
-def favfood(food):
-	print "My favorite food is %s!" % food
-
-favfood("cake")
-
-def enlighten(food):
-	print "The cake is a lie!"
-
-
-
-enlighten("dog")
-
-def console(thing1, option1, option2)
-	print "Just relax! Turn on the %s!" % thing1
+print sphere_volume(5)
+def avg_volume(a, b):
+	c = sphere_volume(a) + sphere_volume(b)
+	return c / 2
+print avg_volume(10, 20)
