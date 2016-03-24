d25f9898359b7664bad6955bfec85f0177060ac0
cmis <cmis@cmis-hs13.(none)>
2016-03-24 08:40:27 +0700

    Worked

--- SimpleProgram.py
+++ SimpleProgram.py
@@ -11,14 +11,17 @@ def define():
 	mother=raw_input("Mother's occupation?(add a/an before):")
 	catch=raw_input("What if your favorite catchphrase?:")
 	friend=raw_input("Who is your best friend?:")
-	thrillride=raw_input("Describe the first thrill ride you ever got on":)
+	thrillride=raw_input("Describe the first thrill ride you ever got on:")
 	oneself=raw_input("Desribe yourself in one word:")
 	favmov=raw_input("What was your reaction to your favorite movie?")
 	kill=raw_input("If you were confronted by a serial killer, how would you act?:")
 
 
 def storytelling():
-	print """
-On a bright sunny day you,{}, was walking down the street. You were thinking about {} and felt {}. You were back from a day of {} at work.  For a long time you wanted to become (Father’s Job), but rethought his choice and decided to be (Mother’s Job). Considers you’ve only got [x] numbers of years left to live, you don’t really have a lt While walking, you see (Crush) across the street, and (best friend) pulling over next to her in a (vehicle of choice).
-	Do you want to confront him or remain where you are?: y/n
-	"""
+	print "On a bright sunny day you, {}, was walking down the street. You were thinking about {} and felt {}. You were back from a day of {} at work. For a long time you wanted to become {}, but rethought his chocie and decided to be {}. Considers you've only got {} numbers of years left to live, you don't really have a lot. While walking, 
+	print "On a bright sunny day you,{}, was walking down the street. You were thinking about {} and felt {}. You were back from a day of {} at work.  For a long time you wanted to become {}, but rethought his choice and decided to be {}. Considers you’ve only got {} numbers of years left to live, you don’t really have a lt While walking, you see {} across the street, and {} pulling over next to her in a {}. Do you want to confront him or remain where you are?: y/n".format(name,crush,favmov,hobby,father,mother,crush,friend,vehicle)
+
+def main():
+	define()
+	storytelling()
+	
