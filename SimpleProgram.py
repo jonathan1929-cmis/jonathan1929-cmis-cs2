dc1d15a93badf9f71aaab51c4113f2f5e80222a3
cmis <cmis@cmis-hs11.(none)>
2016-03-28 10:57:14 +0700

    simpleprogram

--- SimpleProgram.py
+++ SimpleProgram.py
@@ -1,9 +1,12 @@
-def define():
+def storytelling(name,crush,favmov,hobby,father,mother,yearsleft,crush,friend,vehicle):
+	print """On a bright sunny day you, {}, was walking down the street. You were thinking about {} and felt {}. You were back from a day of {} at work. For a long time you wanted to become {}, but rethought his chocie and decided to be {}. Considers you've only got {} numbers of years left to live, you don't really have a lot. While walking, you see {} across the street, and {} pulling over next to her in a {}.""".format(name,crush,favmov,hobby,father,mother,yearsleft,crush,friend,vehicle)
+
+def main():
 	name=raw_input("What is your name?:")
 	age=raw_input("How old are you?:")
 	sex=raw_input("What is your sex?:")
 	hobby=raw_input("What do you enjoy doing in your free time?:")
-	drink=raw_input("What is your favorite drink?")
+	drink=raw_input("What is your favorite drink?:")
 	vehicle=raw_input("What kind of vehicle would you like?:")
 	childhood=raw_input("What was your favorite childhood object?:")
 	crush=raw_input("What is the name of your crush?:")
@@ -13,15 +16,10 @@ def define():
 	friend=raw_input("Who is your best friend?:")
 	thrillride=raw_input("Describe the first thrill ride you ever got on:")
 	oneself=raw_input("Desribe yourself in one word:")
-	favmov=raw_input("What was your reaction to your favorite movie?")
+	favmov=raw_input("What was your reaction to your favorite movie?:")
 	kill=raw_input("If you were confronted by a serial killer, how would you act?:")
+	yearsleft=75-int(age)
+	storytelling(name,crush,favmov,hobby,father,mother,yearsleft,crush,friend,vehicle)
 
+main()
 
-def storytelling():
-	print "On a bright sunny day you, {}, was walking down the street. You were thinking about {} and felt {}. You were back from a day of {} at work. For a long time you wanted to become {}, but rethought his chocie and decided to be {}. Considers you've only got {} numbers of years left to live, you don't really have a lot. While walking, 
-	print "On a bright sunny day you,{}, was walking down the street. You were thinking about {} and felt {}. You were back from a day of {} at work.  For a long time you wanted to become {}, but rethought his choice and decided to be {}. Considers you’ve only got {} numbers of years left to live, you don’t really have a lt While walking, you see {} across the street, and {} pulling over next to her in a {}. Do you want to confront him or remain where you are?: y/n".format(name,crush,favmov,hobby,father,mother,crush,friend,vehicle)
-
-def main():
-	define()
-	storytelling()
-	
