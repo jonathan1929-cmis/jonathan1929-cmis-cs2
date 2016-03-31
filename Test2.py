884f8b87e63d12a2f1c7a55aa6e48ee764802cd1
cmis <cmis@cmis-hs11.(none)>
2016-03-31 08:39:34 +0700

    Tried to code

---
+++ Test2.py
@@ -0,0 +1,69 @@
+#PART 1: Terminology
+#1) Give 3 examples of boolean expressions.
+#a) bool(a,b)
+#b) bool(a+b)
+#c) bool(a)
+#
+#2) What does 'return' do?
+#Return in basic terms is the command whildst defining a function that restates
+#its value or variable. It is used to have the definition function output a
+#specific value or output for use later.
+#
+#3) What are 2 ways indentation is important in python code?
+#a)When defining a function, it is important you don't forget it or else it
+#will result in a syntax error.
+#b)It's a way to help organise your code so groups of code with specific tasks
+#are easier to distinguish.
+#
+
+#PART 2: Reading
+#Type the values for 9 of the 12 of the variables below.
+#
+#problem1_a)
+#problem1_b)
+#problem1_c)
+#problem1_d)
+#
+#problem2_a)True
+#problem2_b)True
+#problem2_c)True
+#problem2_d)False
+#
+#problem3_a)c
+#problem3_b)b
+#problem3_c)c
+#problem3_d)a
+#
+#problem4_a)24
+#problem4_b)6
+#problem4_c)1.5
+#problem4_d)5.5
+#
+
+#PART 3: Programming
+#Write a script that asks the user to type in 3 different numbers.
+#If the user types 3 different numbers the script should then print out the
+#largest of the 3 numbers.
+#If they don't, it should print a message telling them they didn't follow 
+#the directions.
+#Be sure to use the program structure you've learned (main function, processing function, output function)
+
+
+def output():
+
+
+
+def function():
+	print "You didn't follow directions."
+
+
+
+def main():
+	print "Type in 3 different numbers (decimals are OK!)"
+	A=raw_input("A:")
+	B=raw_input("B:")
+	C=raw_input("C:")
+	function()
+	print A
+
+main()
