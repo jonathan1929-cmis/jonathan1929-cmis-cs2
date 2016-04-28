37d5b5fbec22fc4599edc87f89946a6eb695bb65
cmis <cmis@cmis-hs12.(none)>
2016-04-28 13:22:36 +0700

    Finished adder and biggest

--- adder.py
+++ adder.py
@@ -1,18 +1,40 @@
 import math
 
-def main():
-	adder(0)
-
 
 def adder(total):
 	added = raw_input("Next Number: ")
-	if float(added) > 0:
-		total += float(added)
-		print "Running Total: {}".format(float(total))
-		adder(total)
+	if added == "":
+            print "Your total runscore is: {}".format(total)
 	else:
-		print "Your total run score is: {}".format(float(total))
+            total+=float(added)
+            print "Running Total: {}".format(total)
+            adder(total)
 
 
+def main():
+	total = 0
+	adder(total)
+
+main()
+
+
+def adderbig(total, biggest):
+	added = raw_input("Next Number: ")
+	if added == "":
+            print "Your highest runscore was: {}".format(biggest)
+	else:
+		total+=float(added)
+		print "Running Total: {}".format(total)
+		if added > total:
+			biggest = added
+		adderbig(total, biggest)
+
+
+
+
+def main():
+	total = 0
+	biggest = 0
+	adderbig(total, biggest)
 
 main()
