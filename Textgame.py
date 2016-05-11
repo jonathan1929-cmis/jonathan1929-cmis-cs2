097803511a6de7b050c146484f49c6168e3dd068
cmis <cmis@cmis-hs16.(none)>
2016-05-11 13:28:36 +0700

    HEY

---
+++ Textgame.py
@@ -0,0 +1,31 @@
+#This text game will take the player across a death game within a school. There are a number of different members taking place in this game, the aim being to be the last man standing. If a student is killed during this time the other students will try to figure out who the guilty person is. If they get it right, then the person guilty will be killed. If wrong, however, the rest will be killed.
+def main():
+	introduction()
+	start()
+
+
+def introduction():
+	print "Hello! You have been captured and placed in a game where you aim to be the last man standing! You and the other contestants have been placed in a school, with no way out. The game will not end until someone has won. The school has all appliances and daily goods that are required to live, so the only way to get out is to win the game or die of old age, or die by someone elses hand."
+	print """Type in "start!" to start on this adventure!"""
+
+
+def start():
+	print """You suddenly find yourself in a school gymnasium. You have no memory prior except the fact that you must have fainted when entering this school that you just got accepted to.
+You see 14 other members in the gym, equally confused as you are. A voice comes through the speakers of the gym. It sound like a child's. You're told what is happening and the game's rules are explained to you.
+1.  Students may reside only within the school. Leaving campus is an unacceptable use of time.
+2. "Nighttime" is from 10 pm to 7 am. Some areas are off-limits at night, so please exercise caution.
+3. Sleeping anywhere other than the dormitory will be seen as sleeping in class and punished accordingly.
+4. With minimal restrictions, you are free to explore the school at your discretion.
+5. Destruction of surveillance cameras is prohibited.
+6. Anyone who kills a fellow student and becomes "blackened" will graduate(win the game), unless they are discovered.
+7. Additional school regulations may be added if necessary.
+The only tool you are given for the game is a codex of some kind with information involving the members and game rules. The rest of the school is available for your use.
+The introduction ends and you're left with the other members.
+What do you do first?"""
+
+
+def first():
+	print """Type "codex" to view codex
+You have a few choices: run to your room and lock the door, talk with the members, kill someone."""
+	choice1 = raw_input(":")
+	
