2a5b10d26986269af46e080bd9a32fbe7d4b9dd7
cmis <cmis@cmis-hs12.(none)>
2016-05-23 15:16:50 +0700

    met requirements

--- conditional.py
+++ conditional.py
@@ -3,12 +3,12 @@ import math
 import random
 
 def game():
-	opp = random.randint(1, 3)
-	if opp == 1:
+	opp = random.randint(-4, 5)
+	if opp <= 1:
 		opponent = 1		
 	elif opp == 2:
 		opponent = 2	
-	elif opp == 3:
+	elif opp >= 3:
 		opponent = 3
 	return opponent
 
@@ -26,12 +26,17 @@ def initiate():
 def calc(yourhand, opponenthand):
 	if yourhand == 3 and opponenthand == 1 or yourhand == 1 and opponenthand == 2:
 		print "Opponent Wins!"
+		congratsnot = ()
 	elif yourhand == 2 and not opponenthand != 3:
 		print "Opponent Wins!"
+		congratsnot = ()
 	elif yourhand == opponenthand:
 		print "It's a tie!"
+		congratsnot = ()
 	else:
 		print "You Win!"
+		congratsnot = True
+	return congratsnot
 
 def printopp(opponenthand):
 	if opponenthand == 1:
@@ -41,15 +46,24 @@ def printopp(opponenthand):
 	elif opponenthand == 3:
 		return "scissors"
 
+def congrats(congratsnot):
+	if True:
+		return "Congratulations!"
+	else:
+		return "Try again!"
+
+def repeat(congratsnot):
+	if not congratsnot:
+		main()
 
 def main():
 	print """A challenger has appeared!
-You participate in a rock paper scissors duel! Choose your hand!"""
+You participate in a rock paper scissors duel! Choose your hand!"""	
 	yourhand = initiate()
 	opponenthand = game()
 	print "Opponent chose {}!".format(printopp(opponenthand))
-	calc(yourhand, opponenthand)
+	print congrats(calc(yourhand, opponenthand))
 	print "You earned {} points!".format(round(random.random()*80))
-
+	repeat(calc(yourhand, opponenthand))
 
 main()
